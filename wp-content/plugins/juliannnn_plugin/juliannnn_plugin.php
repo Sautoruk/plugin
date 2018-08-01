ba09106370bce4c2881a3796f62fae3ef732c127
Sautoruk <37313067+Sautoruk@users.noreply.github.com>
2018-07-31 21:57:03 -0500

    octavasubida

--- wp-content/plugins/juliannnn_plugin/juliannnn_plugin.php
+++ wp-content/plugins/juliannnn_plugin/juliannnn_plugin.php
@@ -49,6 +49,11 @@ class JulianPlugin
 	function __construct(){
 		add_action('init',array($this,'custom_post_type') );
 	}
+
+	function register(){
+		add_action('admin_enqueue_scripts',array($this,'enqueue') ); //backend y wp para frontend
+	}
+
 	function activate(){
 		//generated a Custom Post Type
 		$this->custom_post_type();
@@ -60,16 +65,22 @@ class JulianPlugin
 	function deactivate(){
 		//flush rewrite rules
 		flush_rewrite_rules();
-
 	}
 
 	function custom_post_type(){
 		register_post_type('book',['public' => true,'label' => 'Books']);
 	}
+
+	function enqueue(){
+		//enqueue all our scripts
+		wp_enqueue_style('mypluginstyle', plugins_url('/assets/mystyle.css',__FILE__));
+		wp_enqueue_script('mypluginscript', plugins_url('/assets/myscript.js',__FILE__));
+	}
 }
 
 if(class_exists('JulianPlugin')){
 	$julianPlugin = new JulianPlugin();
+	$julianPlugin->register();
 }
 
 //activation
